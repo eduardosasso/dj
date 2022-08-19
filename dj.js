901f3fd0c2404c8fa811f9ae4d2afb4d61e09ffd
eduardosasso <eduardo.sasso@gmail.com>
2022-08-18 21:30:32 -0700

    basic types checking

--- dj.js
+++ dj.js
@@ -1,6 +1,38 @@
-import util from "util";
+import _ from "lodash-es";
 
 export const dj = (json) => {
-  const x = JSON.parse(json); // console.log(json);
-  console.log(util.inspect(x, { colors: true }));
+  const data = JSON.parse(json);
+
+  // TODO
+  // if then next if function returns true
+  // add metadata
+  return describe(data);
+};
+
+const describe = (data) => {
+  Object.keys(data).forEach((key) => {
+    formatString(data, key);
+    formatNumber(data, key);
+    formatObject(data, key);
+  });
+
+  return data;
+};
+
+const formatString = (data, key) => {
+  if (!_.isString(data[key])) return;
+
+  data[key] = "string";
+};
+
+const formatNumber = (data, key) => {
+  if (!_.isNumber(data[key])) return;
+
+  data[key] = "number";
+};
+
+const formatObject = (data, key) => {
+  if (!_.isObject(data[key])) return;
+
+  data[key] = describe(data[key]);
 };
--- test/dj.test.js
+++ test/dj.test.js
@@ -1,5 +1,42 @@
 import { expect, test } from "vitest";
+import { dj } from "../dj.js";
 
-test("JSON", () => {
-  expect(1).toBe(2);
+test.skip("primitive types", () => {
+  const data = {
+    name: "John",
+    lastname: "Doe",
+    age: 30,
+    active: true,
+  };
+
+  const json = JSON.stringify(data);
+  const result = dj(json);
+
+  expect(result.name).toBe("string");
+  expect(result.lastname).toBe("string");
+  expect(result.age).toBe("number");
+});
+
+test("object type", () => {
+  const data = {
+    name: "John",
+    lastname: "Doe",
+    age: 30,
+    active: true,
+    address: {
+      street: "Main Street",
+      number: 123,
+    },
+  };
+
+  const json = JSON.stringify(data);
+  const result = dj(json);
+
+  expect(result.name).toBe("string");
+  expect(result.lastname).toBe("string");
+  expect(result.age).toBe("number");
+  expect(result.address.street).toBe("string");
+  expect(result.address.number).toBe("number");
+
+  console.log(result);
 });
